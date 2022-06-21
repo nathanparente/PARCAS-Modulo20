2dc8d89a2d3392b4f63301b017dc907d1a42309d
Nathan Parente <nathan.parente22@gmail.com>
2022-06-21 09:45:28 -0300

    Aula04

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {useNavigate} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-function Home() {
+function Home(props) {
 
     let navigate = useNavigate();
 
@@ -11,12 +12,47 @@ function Home() {
         }, 2000)
     }
 
+    const handleSetarNome = () => {
+        props.setName('Nathan')
+    }
+
+    const handleIncrement = () => {
+        props.increment()
+    }
     return(
         <div>
             <h4>Página HOME</h4>
+
+            NOME: {props.name}<br/>
+            CONTAGEM: {props.contador}<br/><br/>
+
+            <button onClick={handleSetarNome}>Setar nome para: </button>
+            <button onClick={handleIncrement}>+1</button>
+
+            <br/><br/>
             <button onClick={handleButton}>Ir para a página Sobre</button>
         </div>
     )
 }
 
-export default Home;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setName: (newName) => dispatch ({
+            type: 'SET_NAME',
+            payload: {name:newName}
+        }),
+        increment:() => dispatch({
+            type: 'INCREMENT_CONTADOR'
+        })
+    }
+}
+
+
+const mapStateToProps = (state) => {
+    return {
+        name:state.usuario.name,
+        contador:state.usuario.contador
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
--- src/reducers/UsuarioReducer.js
+++ src/reducers/UsuarioReducer.js
@@ -1,14 +1,18 @@
 const initialState = {
-    name:''
-}
+    name:'VISITANTE',
+    contador:0
+};
 
 
 const UsuarioReducer = (state = initialState, action ) =>{
 
-    switch(action. type){
+    switch(action.type){
         case 'SET_NAME':
             return{...state, name: action.payload.name};
         break;
+        case 'INCREMENT_CONTADOR':
+            let newCount = state.contador + 1;
+            return{...state, contador:newCount};
     }
 
     return state
